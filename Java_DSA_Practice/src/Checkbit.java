a9ea122391ffb8f60e45c15c6162b827f3bcddd4
abc <abc@example.com>
2025-01-17 16:31:00 +0530

    Second Comming

--- Java_DSA_Practice/src/Checkbit.java
+++ Java_DSA_Practice/src/Checkbit.java
@@ -1,11 +1,15 @@
 public class Checkbit {
-    public int solve(int A, int B) {
-        return ((A & (1 << B)) !=0) ? 1:0;
-    }
     public static void main(String[] args) {
         Checkbit solution = new Checkbit();
 
         System.out.println(solution.solve(4, 1));
         System.out.println(solution.solve(5, 2));
+    }
+    public int solve(int A, int B) {
+        int num = 1 << B;
+        if((A & num) == 0){
+            return 0;
+        }
+        return 1;
     }
 }
