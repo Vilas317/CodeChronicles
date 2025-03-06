cb35c383c24d936e230f2cfdbbbcabff5364f698
Vilas Tamrakar <Vilashtamrakar@gmail.com>
2025-03-06 23:27:57 +0530

    First Commit

---
+++ JavaScript/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as new JS ver
+
+// alert("hello") // we are using node js not browser
+
+console.log(3 + 3) 
+// code readability should be high
+console.log("abc")
+
+let name = "Ratchet"
+let age = 19
+let isLoggedIn = true
+let state; //undefined
+let country = null //null
+// number => 2 to pow53(range)
+// bigint for big numbers
+// string => use in ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is yet to assign
+// symbol => unique
+// object
+console.log(typeof undefined); // define datatype
